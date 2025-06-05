a4eb0396670b18e39b8148a6a6a6f0c756d05bb6
Ana Rodrigues <163142109+anaprodriguesuk@users.noreply.github.com>
2025-06-05 18:57:37 +0100

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,21 @@
+# ultilizing range to determine the numbers of times the program will run. 
+for i in range(0,11):
+    
+    ''' If-else statment inside a for loop to print a partten:
+    'if' prints out the symble '*' all the times 'i' is less than 5.
+    'else' prints out '*' the rest of times that wasn't print in 'if' minus 10'''
+    
+    if i<=5:
+            print("*" *i)    
+    else:
+            print("*" *(10-i))
+   
+  
+        
+        
+    
+
+
+
+
+  
